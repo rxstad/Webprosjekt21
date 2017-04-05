2f782b673a8a3b5090b60d62912a2ac327fca1bd
Glenn Kenneth Linden <gklinden@gmail.com>
2017-04-05 20:06:29 +0200

    fixet datostart i form.php til å starte i dag + formen blir ikke instanceiert før man velger event

--- form.php
+++ form.php
@@ -10,36 +10,72 @@ require("header.php");
 
 <form method="post"  id="register">
     <p>Navn på event/sted:
-			<input type="text" name="name" placeholder="name" onfocus="this.placeholder = ''" >
+			<input type="text" name="name" placeholder="name" onfocus="this.placeholder = ''" required >
     </p>
     <p>Er dette en event eller et sted?:
-        <select id="type" name="type" onchange="checkForEvent(this.value)">
+        <select id="type" name="type" onchange="addtime(this.value)">
             <option id="sted" value="sted" >sted</option>
             <option  id="event" value="event">event</option>
         </select>
-        <br>
-        <input id="tid" style="display: none" type="date" name="tid" placeholder="datetime" min="2017-01-01" max="3017-01-01" onfocus="this.placeholder = ''" >
-
+        <div id="timeCont" style="height: 1px">
+            <div id="tidDiv">
+                <!--  input for dato kommer her              -->
+            </form>
+        </div>
     </p>
             </datalist>
-    <?php
-    //if (get_id('type') == 'event')
-    ?>
+
     <br><br>
             <input  type="submit" name="submit" value="Submit">
 </form>
 
 
 <script>
+    var d = new Date();
+   var dateNow = d.getFullYear()+"-"+d.getDate()+"-"+d.getMonth();
+   var dateMax = d.getFullYear()+100+"-"+d.getDate()+"-"+d.getMonth();
+
+function addtime(type) {
+    var para=document.createElement("p");
+    var text = document.createTextNode("Når starter eventen?:");
+    var input = document.createElement("input");
+    var input2 = document.createElement("input");
+    input.type ="date";
+    input.id="tid";
+    input.name="dato";
+    input.required="true";
+    input.min= dateNow;
+    input.max= dateMax;
+    input2.id="dato";
+    input2.type="time";
+
+
+
+
+    var tilDiv = document.getElementById("tidDiv");
+    if (type === 'event') {
+        tilDiv.appendChild(para);
+        para.appendChild(text);
+        para.appendChild(input);
+        para.appendChild(input2);
+        console.log(para);
+        console.log(tilDiv.childElementCount);
+    } else{
+
+        tilDiv.removeChild(tilDiv.childNodes[tilDiv.childNodes.length-1]);
+    }
+}
 
 function checkForEvent(type) {
     //var type = document.getElementById("type").value;
-    console.log(type);
+    var t = document.getElementById('tidDiv')
 
     if (type === 'event') {
-    document.getElementById('tid').style.display = "block";
+    t.style.display = "block";
+    document.getElementById("tid").attributes["required"]="true";
     } else{
-    document.getElementById('tid').style.display = "none";
+        t.style.display = "none";
+        document.getElementById("tid").attributes["required"]="false";
     }
 }
 </script>
