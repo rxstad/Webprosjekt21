572e0b467dd488169994f23529473b2e5922d2e4
kongestad <rektom16@student.westerdals.no>
2017-04-29 02:03:47 +0200

    Footer fix
    Flyttet på plassen på teksten / mekka at den blir uppercase +
    tekststørrelse

--- footer.php
+++ footer.php
@@ -2,15 +2,13 @@
 	<div class="inner_footer">
 
 				Copyright © <?php echo date("Y"); ?> WesterLife
-				<br><br>
-				MADE IN OSLO, NORWAY
 				<br><br><br>
 				<img src="img/icons/Facebook.png" width="35px" class="footer_social_img">&nbsp;&nbsp;
 				<img src="img/icons/GooglePlus.png" width="35px" class="footer_social_img">&nbsp;&nbsp;
 				<img src="img/icons/Pinterest.png" width="35px" class="footer_social_img">&nbsp;&nbsp;
 				<img src="img/icons/Twitter.png" width="35px" class="footer_social_img">&nbsp;&nbsp;
 				<img src="img/icons/Share.png" width="35px" class="footer_social_img">
-				<br><br>
-				
+				<br><br><br>
+				Made in Oslo, Norway				
 		
 </div>
