d130285a0be4dbb37792524cffefe88464a7b770
kongestad <rektom16@student.westerdals.no>
2017-05-25 16:38:01 +0200

    Arrangementer / småfiks
    Lagt inn arrangementer og småfiks overalt.

--- arrangementer.php
+++ arrangementer.php
@@ -7,6 +7,167 @@ require("header_menu.php");
 
 <body>
 	<div id="header">
+		<div class="outer_header">
+			<div class="inner_header">
+				<h1 class="title">Arrangementer</h1>
+			</div>
+		</div>
 	</div>
+	<div id="main">
 
+	<dl class="accordion">
+
+<dt><a href=""><img src="img/arrangement/grafisk.jpg" class="accordion_img" width="80px"><h2>Utstilling Westerdals Grafisk design</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	<table style="width:100%">
+	  			<tr>
+				    <td>Rosenkrantz gate 21, Oslo</td> 
+				    <td>
+						<h3>Dato: 26.05 til 02.06.2017</h3>
+						Arrangør: Grafill<br><br>
+						Åpningstider: Mandag-onsdag 09.00-17.00, torsdag 09.00-19.00, lørdag-søndag 12.00-16.00.
+						<br><br>
+						Vernissage fredag 26.mai klokka 18.00
+						<br><br>
+						Siste års studenter ved Westerdals som (endelig) er ferdig med en tre års grafisk design utdannelse stiller ut på Grafill 26. mai - 2. juni. Her vises et bredt spekter av arbeider som studentene har gjort under, og utenfor skolegangen. Med fokus på prosess kommer de til å dele feilene, frustrasjonen og gleden som ligger i tre års grafisk design utdannelse. 
+
+						Kom på åpning av utstillingen den 26. mai klokken 18:00!</td>
+	  			</tr>
+	</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/skuespill.jpg" class="accordion_img" width="80px"><h2>Avgangsforestillinger Skuespill</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	<table style="width:100%">
+	  			<tr>
+				    <td>Øvre Slottsgate 3, 0157 Oslo</td> 
+				    <td>
+						<h3>Dato: 30.05 til 02.06.2017</h3>
+						Arrangør: Westerdals Skuespill<br><br>
+						Åpningstid: 21:00
+						<br><br>
+						To forskjellige skuespill vises som avgangsforestilling fra tredjeårsstudentene på skuespill linja. <br><br>
+						Tirsdag 30. mai kl. 20.00: Premiere «Svømmerne»<br>
+						Onsdag 31. mai kl. 20.00: Forestilling «Svømmerne»<br>
+						Torsdag 1. juni kl. 20.00: Premiere «Et land»<br>
+						Fredag 2. juni kl. 20.00: Forestillinger «Et land»<br>
+					</td>
+	  			</tr>
+	</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/musikkfest.jpg" class="accordion_img" width="80px"><h2>Musikkfest Oslo 2017</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	
+	<table style="width:100%">
+  			<tr>
+			    <td>Kart</td> 
+			    <td>Arrangør</td>
+			    <td>Aka. musikkens Dag. Utendørs Live-musikk. Folkefesten i gatene. Over 450 gratiskonserter å velge mellom, og alt på én dag. Hva mer kan man ønske seg?</td>
+  			</tr>
+</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/øl2.jpg" class="accordion_img" width="80px"><h2>Håndverksølfestivalen</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	
+	<table style="width:100%">
+  			<tr>
+			    <td>Kart</td> 
+			    <td>Arrangør</td>
+			    <td>Her serveres øl fra inn- og utland, med utvalg fra over 40 bryggerier. Få med deg tidenes første håndverkssølfestival i Oslo, og nyt interessante ølsmaksopplevelser!</td>
+  			</tr>
+</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/gin.jpg" class="accordion_img" width="80px"><h2>Oslo Ginfestival</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	
+	<table style="width:100%">
+  			<tr>
+			    <td>Kart</td> 
+			    <td>Arrangør</td>
+			    <td>For deg som liker gin, og er bittelitt over gjennomsnittet nysgjerrig. Enkelt og greit.</td>
+  			</tr>
+</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/løp.jpg" class="accordion_img" width="80px"><h2>Sommernattsløpet</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	
+	<table style="width:100%">
+  			<tr>
+			    <td>Kart</td> 
+			    <td>Arrangør</td>
+			    <td>Løp inn i solnedgangens langs brygga. Arrangeres også Afterrun på båt.</td>
+  			</tr>
+</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/piknik.jpg" class="accordion_img" width="80px"><h2>Piknik i Parken</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	
+	<table style="width:100%">
+  			<tr>
+			    <td>Kart</td> 
+			    <td>Arrangør</td>
+			    <td>De fleste har kanskje fått med seg dette, men musikkfestival er så gøy, og fremheve de litt mindre anser vi som vikig - derfor tar vi sjansen. First Aid Kit, Father John Misty, Solange... Kanskje ikke så liten festival likevel?</td>
+  			</tr>
+</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/festival.jpg" class="accordion_img" width="80px"><h2>Høvdingsens Midtsommerfestival</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	
+	<table style="width:100%">
+  			<tr>
+			    <td>Kart</td> 
+			    <td>Arrangør</td>
+			    <td>Bruk den lengste dagen i året til å tilbringe en rytmerik kveld på Månefisken. Program fra 18.00 - 03.00</td>
+  			</tr>
+</table>
+
+</dd>
+
+<dt><a href=""><img src="img/arrangement/pokemon.jpg" class="accordion_img" width="80px"><h2>Pokémonmesse</h2><img src="img/arrangement/arrow.png" class="accordion_arrow" width="25px"></a></dt>
+<dd>
+	
+	<table style="width:100%">
+  			<tr>
+			    <td>Kart</td> 
+			    <td>Arrangør</td>
+			    <td>Om man enten er superfan eller bare er fascinert over dette hjørnet av popkulturen: Pokémonturneringer, stands fra butikker og samlere, konkurranser og byttehjørner - noe for deg?</td>
+  			</tr>
+</table>
+
+</dd>
+
+</dl>
+
+<h1><span>Se flere</span></h1>
+	</div>
+	<?php
+	require("footer.php");
+	?>
+
+	<script>
+(function($) {
+    
+  var allPanels = $('.accordion > dd').hide();
+    
+  $('.accordion > dt > a').click(function() {
+    allPanels.slideUp();
+    $(this).parent().next().slideDown();
+    return false;
+  });
+
+})(jQuery);
+	</script>
 </body>
