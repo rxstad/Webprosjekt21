d65c4729af63addfbb30f577aa99cc894125ca30
kongestad <rektom16@student.westerdals.no>
2017-05-25 12:43:25 +0200

    Arrangement

---
+++ arrangementer.php
@@ -0,0 +1,12 @@
+<?php
+require("header.php");
+require("header_menu.php");
+
+?>
+<link rel="stylesheet" type="text/css" href="css/arrangementer_style.css">
+
+<body>
+	<div id="header">
+	</div>
+
+</body>
